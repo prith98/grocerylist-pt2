cb5f32a03562a48e7e26b3abf6c231578d1e542b
Prith <prith98@gmail.com>
2021-12-13 14:28:14 -0800

    Created GET and POST functions, working with POSTMAN

--- backend/server/index.js
+++ backend/server/index.js
@@ -9,6 +9,33 @@ const PORT = 3000;
 app.use('/', express.static('frontend/dist'));
 app.use(express.json());
 
+app.get('/api/groceries', (req, res) => {
+  db.query('SELECT * FROM groceries', (err, data) => {
+    if (err) {
+      throw new Error(err);
+      res.send(err);
+      return;
+    }
+    console.log('GET REQUEST SUCCESSFUL');
+    res.send(data);
+  })
+});
+
+app.post('/api/groceries', (req, res) => {
+  const {name, quantity, best_before, purchased} = req.body;
+  db.query('INSERT INTO groceries (name, quantity, best_before, purchased) VALUES(?, ?, ?, ?)',
+  [name, quantity, best_before, purchased], (err, data) => {
+    if (err) {
+      throw new Error (err);
+      console.log(err);
+      res.send(err);
+      return;
+    }
+    res.send('Successfully added entry to db');
+  })
+});
+
+
 app.listen(PORT, () => {
   console.log(`I'm listening on port: ${PORT}`);
 })
